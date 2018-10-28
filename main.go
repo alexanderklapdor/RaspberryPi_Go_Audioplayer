2e065be2a5012609d86efa54aa03bb25f73f9aaa
ArianMoser <arian_moser@yahoo.de>
2018-10-28 17:39:34 +0100

    Added functionality to search through given folder (subdirectories and files)

--- main.go
+++ main.go
@@ -7,6 +7,9 @@ import "bufio"
 import "log"
 import "os"
 import "strings"
+import "path/filepath"
+import "path"
+import "io/ioutil"
 //import "strconv"
 
 func main() {
@@ -15,8 +18,7 @@ func main() {
 	fmt.Println("############################################")
 
 	// available Arguments (arguments are pointer!) 
-	inputFile 	:= flag.String("i", "", "input music file") 
-	inputFolder 	:= flag.String("f", "", "input music folder")
+	input 		:= flag.String("i", "", "input music file/folder") 
 	volume 		:= flag.Int("v", 50, 	"music volume in percent (default 50)")
 	shuffle 	:= flag.Bool("s", false,"shuffle (default false)")
 	fadeIn 		:= flag.Int("fi", 0, 	"fadein in milliseconds (default 0)")
@@ -24,8 +26,7 @@ func main() {
 
 	flag.Parse()
 
-	fmt.Println("File:     ", *inputFile)
-	fmt.Println("Folder:   ", *inputFolder)
+	fmt.Println("Input:    ", *input)
 	fmt.Println("Volume:   ", *volume)
 	fmt.Println("Shuffle:  ", *shuffle)
 	fmt.Println("Fade in:  ", *fadeIn)
@@ -38,7 +39,33 @@ func main() {
 	for _, format := range supportedFormats {
 		fmt.Print(" ",format)
 	}
+	fmt.Println()
 
+	// check if file exists
+	fi, err := os.Stat(*input)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("input given")
+	switch mode := fi.Mode();{
+	case mode.IsDir():
+		fmt.Println("Found directory")
+		fileList := getFilesInFolder(*input, supportedFormats,2)
+		fmt.Println("Supported Files: ")
+		for _, fileElement := range fileList {
+			fmt.Println(fileElement)
+		}
+	case mode.IsRegular():
+		fmt.Println("Found file")
+		var extension = filepath.Ext(*input)
+		if stringInArray(extension, supportedFormats) {
+			fmt.Println("Extension supported")
+		} else {
+			fmt.Println("Extension not supported")
+		}
+	}
 
 	fmt.Println("")
 	fmt.Println("********************************************")
@@ -46,6 +73,48 @@ func main() {
 	fmt.Println("********************************************")
 }
 
+func getFilesInFolder(folder string, supportedExtensions []string, depth int) []string {
+	fmt.Println("get files in ", folder)
+	fileList := make([]string, 0)
+	if depth > 0 {
+		files, err := ioutil.ReadDir(folder)
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, file := range files {
+			filename := joinPath(folder,file.Name())
+
+			fi, err := os.Stat(filename)
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			switch mode := fi.Mode(); {
+			case mode.IsDir():
+				newFolder :=  filename + "/"
+				newFiles := getFilesInFolder(newFolder, supportedExtensions, depth-1)
+				for _, newFile := range newFiles {
+					fileList = append(fileList, newFile)
+				}
+			case mode.IsRegular():
+				var extension = filepath.Ext(filename)
+				if stringInArray(extension, supportedExtensions) {
+					fileList = append(fileList, filename)
+				}
+			}
+		}
+	} else {
+		//fmt.Println("Max depth reached")
+	}
+	return fileList
+}
+
+func joinPath(source, target string) string {
+	if path.IsAbs(target) {
+		return target
+	}
+	return path.Join(path.Dir(source), target)
+}
 
 func getSupportedFormats() []string{
 	// get supported audio formats of 'supportedFormats.cfg' file
@@ -76,3 +145,12 @@ func getSupportedFormats() []string{
 }
 
 
+func stringInArray(str string, list []string) bool {
+	// check if string is in string-list
+	for _, element := range list {
+		if element == str {
+			return true
+		}
+	}
+	return false
+}
