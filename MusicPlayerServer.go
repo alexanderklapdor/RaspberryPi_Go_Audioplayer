2765b1f389119cfdfe8f2edf79afeb94a21d8833
Arian Moser <arian.moser@hpe.com>
2019-02-07 17:39:20 +0000

    Added every parameter to be passed and added functions to readout path

--- MusicPlayerServer.go
+++ MusicPlayerServer.go
@@ -3,10 +3,10 @@ package main
 import (
         "log"
         "net"
-        "fmt"
         "encoding/json"
         "os"
         "syscall"
+	"github.com/alexanderklapdor/RaspberryPi_Go_Audioplayer/logger"
 )
 
 type Request struct {
@@ -15,8 +15,12 @@ type Request struct {
 }
 
 type Data struct {
-    Volume int
-    Songs []string
+    Depth   int
+    FadeIn  int
+    FadeOut int
+    Path    string
+    Shuffle bool
+    Volume  int
 }
 
 func receiveCommand(c net.Conn) {
@@ -27,12 +31,16 @@ func receiveCommand(c net.Conn) {
         return
     }
     receivedBytes := buf[0:nr]
-    println("Server received command: ", string(receivedBytes))
-    // convert message back to json
+    logger.Log.Info("Server received message: " + string(receivedBytes))
+
+    // convert message back to a request-object
+    logger.Log.Notice("Converting message back to a Request-Object")
     received := Request{}
     json.Unmarshal(receivedBytes, &received)
     command := received.Command
     data := received.Data
+    logger.Log.Notice("Command: " + command)
+    //logger.Log.Notice("Data   : " + string(data))
 
     // switch case commands
     switch command {
@@ -53,64 +61,65 @@ func receiveCommand(c net.Conn) {
     case "info":
         printInfo()
     default:
-        println("Unknown command")
-    }
-
-
-    if command == "exit" {
-       closeConnection(c)
+        logger.Log.Error("Unknown command received")
     }
 
 
     // write to client
-    _, err = c.Write([]byte("Ok perfect"))
+    logger.Log.Notice("Send a message back to the client")
+    message := "Default-message"
+    _, err = c.Write([]byte(message))
     if err != nil {
         log.Fatal("Write: ", err)
     }
 } // end of receiveCommand
 
 func closeConnection(c net.Conn) {
-    println("Connection will be closed")
+    socketPath := "/tmp/mp.sock" // todo: should be passed as an argument or be written out of a config file
+    logger.Log.Warning("Connection  will be closed")
     defer c.Close()
-    err :=  syscall.Unlink("/tmp/mp2.sock")
+    err :=  syscall.Unlink(socketPath)
     if err != nil {
-        println("Unlink()", err)
+        logger.Log.Error("Error during unlink process of the socket: " + err.Error())
+        logger.Log.Info("Pls run manually unlink 'unlink" + socketPath + "'")
     }
     os.Exit(0)
 } // end of closeConnection
 
 func playMusic(data Data) {
-    println("Play Music")
+    logger.Log.Info("Executing: Play Music")
 }
 
+
 func pauseMusic(data Data) {
-    println("Pause Music")
+    logger.Log.Info("Executing: Pause Music")
 }
 
 func setVolume(data Data) {
-    println("Set Volume")
+    logger.Log.Info("Executing: Set Volume")
 }
 
 func addToQueue(data Data) {
-    println("Add to queue")
+    logger.Log.Info("Executing: Add to queue")
 }
 
 func increaseVolume() {
-    println("Increase volume")
+    logger.Log.Info("Executing: Increase volume")
 }
 
 func decreaseVolume() {
-    println("Decrease volume")
+    logger.Log.Info("Executing: Decrease volume")
 }
 
 func printInfo() {
-    println("Info ")
+    logger.Log.Info("Executing: Print info ")
 }
 
 func main() {
     unixSocket := "/tmp/mp.sock"
     // create server socket mp.sock
-    fmt.Println("Starting Unix socket on '" + unixSocket + "'")
+    logger.Log.Notice("Creating unixSocket.")
+    logger.Log.Info("Listening on " + unixSocket)
     ln, err := net.Listen("unix", unixSocket)
     if err != nil {
         log.Fatal("listen error", err)
@@ -124,3 +133,98 @@ func main() {
         go receiveCommand(conn)
     }
 } // end of main
+
+func parseSongs(path string, depth int) []string {
+    // check if given file/folder exists
+    logger.Log.Notice("Check if folder/file exists", path)
+    fi, err := os.Stat(path)
+    util.Check(err)
+
+    switch mode := fi.Mode(); {
+    case mode.IsDir():
+        // directory given
+        logger.Log.Info("Directory found")
+        logger.Log.Notice("Getting files inside of the folder")
+        fileList := getFilesInFolder(path, supportedFormats, depth)
+        //Print Supported Filelist
+        screener.PrintFiles(fileList, false)
+        return fileList
+    case mode.IsRegular():
+        // file given
+        logger.Log.Notice("File found")
+        var extension = filepath.Ext(path)
+        if util.StringInArray(extension, supportedFormats) {
+            logger.Log.Notice("Extension supported")
+            return path
+        } else {
+            logger.Log.Warning("Extension not supported")
+            return []
+        }
+    default:
+        logger.Log.Error("Path is not a file or a folder")
+        return []
+    } // end of switch
+}
+
+func getFilesInFolder(folder string, supportedExtensions []string, depth int) []string {
+	// fmt.Println("get files in ", folder)
+	fileList := make([]string, 0)
+	if depth > 0 {
+		files, err := ioutil.ReadDir(folder)
+		util.Check(err)
+		for _, file := range files {
+			filename := joinPath(folder, file.Name())
+
+			fi, err := os.Stat(filename)
+			util.Check(err)
+
+			switch mode := fi.Mode(); {
+			case mode.IsDir():
+				newFolder := filename + "/"
+				newFiles := getFilesInFolder(newFolder, supportedExtensions, depth-1)
+				for _, newFile := range newFiles {
+					fileList = append(fileList, newFile)
+				}
+			case mode.IsRegular():
+				var extension = filepath.Ext(filename)
+				if util.StringInArray(extension, supportedExtensions) {
+					fileList = append(fileList, filename)
+				}
+			}
+		}
+	} else {
+		//fmt.Println("Max depth reached")
+	}
+	return fileList
+}
+
+func joinPath(source, target string) string {
+	if path.IsAbs(target) {
+		return target
+	}
+	return path.Join(path.Dir(source), target)
+}
+
+func getSupportedFormats() []string {
+	// get supported audio formats of 'supportedFormats.cfg' file
+	supportedFormats := make([]string, 0)
+
+	// Opening file
+	file, err := os.Open("supportedFormats.cfg")
+	util.Check(err)
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		//fmt.Println(line)
+		if !strings.ContainsAny(line, "#") {
+			supportedFormats = append(supportedFormats, line)
+			//fmt.Println("format", line)
+		}
+	}
+
+	util.Check(scanner.Err())
+	return supportedFormats
+
+}
