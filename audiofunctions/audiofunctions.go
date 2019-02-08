117139b3e4cf67a3b49ccd79b92d359b5bc33700
Arian Moser <arian.moser@hpe.com>
2019-02-08 11:43:32 +0000

    Formatted file

--- audiofunctions/audiofunctions.go
+++ audiofunctions/audiofunctions.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/alexanderklapdor/RaspberryPi_Go_Audioplayer/logger"
 	"github.com/alexanderklapdor/RaspberryPi_Go_Audioplayer/util"
+	"github.com/alexanderklapdor/RaspberryPi_Go_Audioplayer"
 	"github.com/bobertlo/go-mpg123/mpg123"
 	"github.com/gordonklaus/portaudio"
 )
@@ -22,105 +23,111 @@ var stream *portaudio.Stream
 
 // PlayAudio Function
 func PlayAudio(fileName string) {
-	fmt.Println("Playing audiofile.  Press Ctrl-C to stop.")
+    fmt.Println("Playing audiofile.  Press Ctrl-C to stop.")
 
-	sig := make(chan os.Signal, 1)
-	signal.Notify(sig, os.Interrupt, os.Kill)
+    sig := make(chan os.Signal, 1)
+    signal.Notify(sig, os.Interrupt, os.Kill)
 
-	// create mpg123 decoder instance
-	decoder, err := mpg123.NewDecoder("")
-	util.Check(err)
+    // create mpg123 decoder instance
+    decoder, err := mpg123.NewDecoder("")
+    util.Check(err)
 
-	util.Check(decoder.Open(fileName))
-	defer decoder.Close()
+    util.Check(decoder.Open(fileName))
+    defer decoder.Close()
 
-	// get audio format information
-	rate, channels, _ := decoder.GetFormat()
+    // get audio format information
+    rate, channels, _ := decoder.GetFormat()
 
-	// make sure output format does not change
-	decoder.FormatNone()
-	decoder.Format(rate, channels, mpg123.ENC_SIGNED_16)
+    // make sure output format does not change
+    decoder.FormatNone()
+    decoder.Format(rate, channels, mpg123.ENC_SIGNED_16)
 
-	portaudio.Initialize()
-	defer portaudio.Terminate()
-	out := make([]int16, 8192)
-	stream, err = portaudio.OpenDefaultStream(0, channels, float64(rate), len(out), &out)
-	util.Check(err)
-	defer stream.Close()
+    portaudio.Initialize()
+    defer portaudio.Terminate()
+    out := make([]int16, 8192)
+    stream, err = portaudio.OpenDefaultStream(0, channels, float64(rate), len(out), &out)
+    util.Check(err)
+    // todo: here call next song
+    defer callNextSong()
+    defer stream.Close()
 
-	util.Check(stream.Start())
-	defer stream.Stop()
-	for {
-		if needPause != false {
-			stream.Stop()
-			for needPause == true {}
-			stream.Start()
-		}
-		if needStop != false {return}
-		audio := make([]byte, 2*len(out))
-		_, err = decoder.Read(audio)
-		if err == mpg123.EOF {
-			break
-		}
-		util.Check(err)
+    util.Check(stream.Start())
+    defer stream.Stop()
+    for {
+        if needPause != false {
+            stream.Stop()
+            for needPause == true {}
+            stream.Start()
+        }
+        if needStop != false {return}
+        audio := make([]byte, 2*len(out))
+        _, err = decoder.Read(audio)
+        if err == mpg123.EOF {
+            break
+        }
+        util.Check(err)
 
-		util.Check(binary.Read(bytes.NewBuffer(audio), binary.LittleEndian, out))
-		util.Check(stream.Write())
-		select {
-		case <-sig:
-			return
-		default:
-		}
-	}
-}
+        util.Check(binary.Read(bytes.NewBuffer(audio), binary.LittleEndian, out))
+        util.Check(stream.Write())
+        select {
+            case <-sig:
+                return
+            default:
+        }
+    } // end of for
+} // end of PlayAudio
+
+func CallNextSong() {
+    fmt.Println("CallNextSong")
+} // end of CallNextSong
 
 // StopAudio Function
 func StopAudio() {
-	needStop = true
-}
+    needStop = true
+} // end of StopAudio
 
 //PauseAudio Function
 func PauseAudio() {
-	needPause = true
-}
+    needPause = true
+} // end of PauseAudio
 
 // ResumeAudio Function
 func ResumeAudio() {
-	needPause = false
-}
+    needPause = false
+} // end of ResumeAudio
 
 // SetVolume Function
 func SetVolume(volumeValue string) {
-	cmd := exec.Command("amixer", "set", "Master", volumeValue+"%")
-	err := cmd.Run()
-	if err != nil {
-		logger.Log.Error("SetVolume failed with " + err.Error() + "\n")
-	}
-}
+    cmd := exec.Command("amixer", "set", "Master", volumeValue+"%")
+    err := cmd.Run()
+    if err != nil {
+        logger.Log.Error("SetVolume failed with " + err.Error() + "\n")
+    }
+} // end of SetVolume
 
 // SetVolumeUp Function
 func SetVolumeUp() {
-	cmd := exec.Command("amixer", "set", "Master", "2%+")
-	err := cmd.Run()
-	if err != nil {
-		logger.Log.Error("SetVolumeUp failed with " + err.Error() + "\n")
-	}
-}
+    cmd := exec.Command("amixer", "set", "Master", "2%+")
+    err := cmd.Run()
+    if err != nil {
+        logger.Log.Error("SetVolumeUp failed with " + err.Error() + "\n")
+    }
+} // end of SetVolumeUp
 
 // SetVolumeDown Function
 func SetVolumeDown() {
-	cmd := exec.Command("amixer", "set", "Master", "2%-")
-	err := cmd.Run()
-	if err != nil {
-		logger.Log.Error("SetVolumeDown failed with " + err.Error() + "\n")
-	}
-}
+    cmd := exec.Command("amixer", "set", "Master", "2%-")
+    err := cmd.Run()
+    if err != nil {
+        logger.Log.Error("SetVolumeDown failed with " + err.Error() + "\n")
+    }
+} //end of SetVolumeDown
 
 // StartPulseaudio Function
 func StartPulseaudio() {
-	cmd := exec.Command("pulseaudio", "-D")
-	err := cmd.Run()
-	if err != nil {
-		logger.Log.Error("StartPulseaudio failed with " + err.Error() + "\n")
-	}
-}
+    cmd := exec.Command("pulseaudio", "-D")
+    err := cmd.Run()
+    if err != nil {
+        logger.Log.Error("StartPulseaudio failed with " + err.Error() + "\n")
+    }
+} // end of StartPulseaudio
