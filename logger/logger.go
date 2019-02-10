3c4a50948b5689e599780aefc22343f4073ee1b3
Alexander Klapdor <alekla1@web.de>
2019-02-10 18:31:34 +0100

    Change Logger - Log to File

--- logger/logger.go
+++ logger/logger.go
@@ -1,24 +1,148 @@
 package logger
 
-//Imports
-import "os"
-import "github.com/op/go-logging"
-
-// Var Definition
-var Log = logging.MustGetLogger("Test")
-var format = logging.MustStringFormatter(
-	`%{color}%{time:15:04:05.000} %{shortfunc} -> %{level:.4s} %{id:03x}%{color:reset} %{message}`,
+// Imports
+import (
+	"fmt"
+	"os"
+	"time"
 )
 
-func SetUpLogger() {
-	// Create backend for os.Stderr.
-	backend1 := logging.NewLogBackend(os.Stderr, "", 0)
-	backend1Formatter := logging.NewBackendFormatter(backend1, format)
+// Var Definition
+var file *os.File
+var filename = "./Log.txt"
+var debug = false
 
-	// Only errors and more severe messages should be sent to backend1
-	backend1Leveled := logging.AddModuleLevel(backend1)
-	backend1Leveled.SetLevel(logging.ERROR, "")
+// Setup Logger
+func Setup(filepath string, debugMode bool) {
+	filename = filepath
+	debug = debugMode
+}
 
-	// Set the backends to be used.
-	logging.SetBackend(backend1Leveled, backend1Formatter)
+// Debug Option
+func Debug(message string) {
+	//check file
+	checkFile()
+	//open file
+	openFile()
+	//close file after writing
+	defer file.Close()
+	//append to file
+	appendString("DEBU", message)
+	//print to stdoutput if debug is true
+	if debug == true {
+		fmt.Println("DEBUG -> " + message)
+	}
+}
+
+// Info Option
+func Info(message string) {
+	//check file
+	checkFile()
+	//open file
+	openFile()
+	//close file after writing
+	defer file.Close()
+	//append to file
+	appendString("INFO", message)
+	//print to stdoutput if debug is true
+	if debug == true {
+		fmt.Println("INFO -> " + message)
+	}
+}
+
+// Notice Option
+func Notice(message string) {
+	//check file
+	checkFile()
+	//open file
+	openFile()
+	//close file after writing
+	defer file.Close()
+	//append to file
+	appendString("NOTI", message)
+	//print to stdoutput if debug is true
+	if debug == true {
+		fmt.Println("NOTICE -> " + message)
+	}
+}
+
+// Warning Option
+func Warning(message string) {
+	//check file
+	checkFile()
+	//open file
+	openFile()
+	//close file after writing
+	defer file.Close()
+	//append to file
+	appendString("WARN", message)
+	//print to stdoutput if debug is true
+	if debug == true {
+		fmt.Println("WARNING -> " + message)
+	}
+}
+
+// Error Option
+func Error(message string) {
+	//check file
+	checkFile()
+	//open file
+	openFile()
+	//close file after writing
+	defer file.Close()
+	//append to file
+	appendString("ERRO", message)
+	//log to stdoutput
+	fmt.Println("ERROR -> " + message)
+
+}
+
+// Critical Option
+func Critical(message string) {
+	//check file
+	checkFile()
+	//open file
+	openFile()
+	//close file after writing
+	defer file.Close()
+	//append to file
+	appendString("CRIT", message)
+	//log to stdoutput
+	fmt.Println("CRITICAL -> " + message)
+	panic(message)
+}
+
+func checkFile() {
+	if _, err := os.Stat("./Log.txt"); err == nil {
+		//file exists
+	} else if os.IsNotExist(err) {
+		// file not exists -> create file
+		file, err = os.Create(filename)
+		check(err)
+	} else {
+		// Schrodinger: file may or may not exist. See err for details.
+		fmt.Println(err)
+		panic(err)
+	}
+}
+
+func openFile() {
+	//open file
+	var err error
+	file, err = os.OpenFile(filename, os.O_APPEND, 0600)
+	check(err)
+}
+
+func appendString(mType string, message string) {
+	//append to file
+	var err error
+	if _, err = file.WriteString(time.Now().Format("2006.01.02 15:04:05") + " " + mType + " -> " + message + "\n"); err != nil {
+		panic(err)
+	}
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
 }
