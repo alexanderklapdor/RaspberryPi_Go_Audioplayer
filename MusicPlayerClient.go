a2b9160e0161f4a5242847b7816c6c86cd5a1f24
Arian Moser <arian.moser@hpe.com>
2019-02-11 17:55:41 +0000

    Auto start Server inside of the client, if server isnt already up

--- MusicPlayerClient.go
+++ MusicPlayerClient.go
@@ -5,10 +5,14 @@ import (
 	"flag"
 	"os"
 	"strconv"
+        "os/exec"
+        "strings"
+        "time"
 
 	"github.com/alexanderklapdor/RaspberryPi_Go_Audioplayer/logger"
 	"github.com/alexanderklapdor/RaspberryPi_Go_Audioplayer/screener"
 	"github.com/alexanderklapdor/RaspberryPi_Go_Audioplayer/sender"
+	"github.com/alexanderklapdor/RaspberryPi_Go_Audioplayer/util"
 	// "github.com/alexanderklapdor/RaspberryPi_Go_Audioplayer/util"
 )
 
@@ -34,9 +38,35 @@ func main() {
 	// Set up Logger
         //todo: logging path in configuration file
 	logger.Setup("logs/client.log", true)
+        socket_path := "/tmp/mp.sock" // todo: config file socket path
 
 	// Start Screen
 	screener.StartScreen()
+
+        // check if server is running
+        if checkServerStatus() {
+                logger.Info("Server is running")
+        } else {
+                logger.Info("Server is not running")
+                startServer()
+                ind := 0
+                for {
+                        if _, err := os.Stat(socket_path); err == nil || ind > 10{
+                                break
+                        } // end of if
+                        logger.Info("Waiting for server")
+                        time.Sleep(1 * time.Second)
+                } // end of for
+                if _, err2 := os.Stat(socket_path); err2 == nil {
+                        logger.Info("Server started succesfully")
+                } else if os.IsNotExist(err2){
+                        logger.Info("Server not started succesfully" )
+                        os.Exit(304)
+                } else {
+                        logger.Info("Something unexpected happened")
+                        os.Exit(777)
+                }
+        } // end of else
 
 	// check if no argument is given
 	if len(os.Args) < 2 {
@@ -86,7 +116,7 @@ func main() {
 
 	// print received argument
 	logger.Notice("Given arguments:")
-	logger.Info("Commabd   " + *command)
+	logger.Info("Command   " + *command)
 	logger.Info("Input:    " + *input)
 	logger.Info("Volume:   " + strconv.Itoa(*volume))
 	logger.Info("Depth:    " + strconv.Itoa(*depth))
@@ -116,6 +146,42 @@ func main() {
 	requestJson, _ := json.Marshal(requestInfo)
 	logger.Info("JSON String : " + string(requestJson))
 
-	sender.Send(requestJson)
+	sender.Send(requestJson) //todo: socket should be given to the sender
 
+}
+
+func checkServerStatus() bool{
+        socket_path := "/tmp/mp.sock"
+        if _, err := os.Stat(socket_path); err != nil {
+                return false //unix socket does not exists
+        } else {
+                // check if process exists
+                cmd := "ps -ef | grep MusicPlayerServer"
+                output, err := exec.Command("bash", "-c", cmd).Output()
+                util.Check(err)
+                for _, pi := range strings.Split(string(output), "\n") {
+                        if strings.Contains(pi, "go run") {
+                                return true
+                        } // end of if
+                } // end of for
+                return false
+        } // end of else
+} // end of checkServerStatus
+
+func startServer() {
+        logger.Info("Starting Server process")
+        var attr = os.ProcAttr{
+            Dir: ".",
+            Env: os.Environ(),
+            Files: []*os.File{
+                os.Stdin,
+                nil,
+                nil,
+            },
+        }
+        process, err := os.StartProcess("/usr/local/go/bin/go", []string{"go", "run", "MusicPlayerServer.go"}, &attr)
+        util.Check(err)
+        logger.Info("Detaching process")
+        err = process.Release()
+        util.Check(err)
 }
